81632977eb14eadc6e465aecf196c962398446fa
JanderC <janderalexisc@gmail.com>
2025-12-19 11:47:02 -0400

    cambios en el diseño para el nuevo gimnasio

--- obj/Debug/net8.0/VidaFit.AssemblyInfo.cs
+++ obj/Debug/net8.0/VidaFit.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VidaFit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96144f72d40f07021c038a961940fb775d062c56")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8a21831f8bb4d7857df26693ae27b41b563dba4")]
 [assembly: System.Reflection.AssemblyProductAttribute("VidaFit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VidaFit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
