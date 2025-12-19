69b1a5973120b91f4976f72abef34a6b26412bc2
JanderC <janderalexisc@gmail.com>
2025-12-19 15:53:44 -0400

    cambios

--- obj/Debug/net8.0/VidaFit.AssemblyInfo.cs
+++ obj/Debug/net8.0/VidaFit.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VidaFit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f4d4d778e6407fd1c94ded4adf43bfc72798536")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f123b4abeb594ee5c6637eaf4a4183c7e7a1014")]
 [assembly: System.Reflection.AssemblyProductAttribute("VidaFit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VidaFit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
