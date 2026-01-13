0888c3209196732fa324fb6ee99182c4470b3694
JanderC <janderalexisc@gmail.com>
2026-01-12 21:27:42 -0400

    cambios

--- obj/Debug/net8.0/VidaFit.AssemblyInfo.cs
+++ obj/Debug/net8.0/VidaFit.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VidaFit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba76f1157d3dda576a1231d26c6e3a4b0b84709f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9d02ab844c8f0409ca08d97550a023f5bfcb231")]
 [assembly: System.Reflection.AssemblyProductAttribute("VidaFit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VidaFit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- obj/Release/net8.0/win-x64/VidaFit.AssemblyInfo.cs
+++ obj/Release/net8.0/win-x64/VidaFit.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VidaFit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+184bfee9451930d254297620ae801d2b85ac791f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9d02ab844c8f0409ca08d97550a023f5bfcb231")]
 [assembly: System.Reflection.AssemblyProductAttribute("VidaFit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VidaFit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
