613cb89def8c6041c51baccdead47e62ac36f0fb
JanderC <janderalexisc@gmail.com>
2026-02-12 11:38:36 -0400

    cambios

--- obj/Release/net8.0/win-x64/VidaFit.AssemblyInfo.cs
+++ obj/Release/net8.0/win-x64/VidaFit.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VidaFit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20b23dc3ccda1bab42427494de015492560e2b9b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee48f91e9c2ab198d29dd15364ce8f6c8d9c4a02")]
 [assembly: System.Reflection.AssemblyProductAttribute("VidaFit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VidaFit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
